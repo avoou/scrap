f47f1bd1adc3359fe98d30a0b17ba7ff326f921c
avoou <xxxenodochialritchie@gmail.com>
2024-04-19 11:32:46 +0300

    add price, current, link column

--- krossy.py
+++ krossy.py
@@ -1,24 +1,72 @@
 import requests
-from bs4 import BeautifulSoup
+from pprint import pprint
+from bs4 import BeautifulSoup, Tag
 
-results_ua_url = 'https://megasport.ua/ua/catalog/krossovki-i-snikersi/male/'
-response = requests.get(results_ua_url)
-print(response.encoding)
-response.encoding = 'utf-8'
-txt = response.text
-sp = BeautifulSoup(txt, 'html.parser')
+
+def get_bs_by_url(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    txt = response.text
+    return BeautifulSoup(txt, 'html.parser')
+
+
+def get_name(item: Tag) -> str:
+    try:
+        name = item.find('div', class_='ihuxuw').text
+        name = name.replace("\u2009", " ").replace("\xa0", " ")
+        return name
+    except Exception:
+        return None
+
+
+def get_price_current(item: Tag) -> tuple[int, str]:
+    try:
+        price = item.find('span', class_='MeSmTt').text
+        price, current = price.split("\u2009")
+        price = int(price.replace("\xa0", ""))
+
+        return price, current
+    except Exception:
+        return None, None
+
+
+def get_items_link(item: Tag):
+    try:
+        tag = item.find('a', class_='it25hX')
+        return tag.get('href')
+    except Exception:
+        return None
+
+
+def get_pages_count(sp: Tag) -> int:
+    return len(sp.find(id='select-page'))
+
+host = 'https://megasport.ua'
+boots_url = '/ua/catalog/krossovki-i-snikersi/male/'
+
+sp = get_bs_by_url(host + boots_url)
 
 pages = len(sp.find(id='select-page'))
 
-items = sp.find('div', class_='Fkfp3V')
+collect = []
 
-items = [item for item in items]
-item = items[1]
-name = item.find('div', class_='ihuxuw').text
-price = item.find('span', class_='MeSmTt').text
-price = price.replace("\xa0", " ")
-price = price.split(' грн')[0]
-price = price.replace(" ", "")
-price = int(price)
-print(name, price)
-print(len(sp.find(id='select-page')))
+for i in range(1, 2):
+    url = host + boots_url + f"page-{i}/"
+    sp = get_bs_by_url(url)
+    items = sp.find('div', class_='Fkfp3V')
+    
+    for item in items:
+        name = get_name(item=item)
+        price, current = get_price_current(item=item)
+        link = get_items_link(item)
+        res = {
+            "name": name,
+            "price": price,
+            "current": current,
+            "link": host + str(link),
+        }
+        collect.append(res)
+
+
+pprint(collect)
+print(len(collect))
