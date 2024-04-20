cb2efb814a3959a0a1fa2cd9a89bd566c9ab1841
avoou <xxxenodochialritchie@gmail.com>
2024-04-20 13:20:45 +0300

    change abc class

--- krossy.py
+++ krossy.py
@@ -1,37 +1,9 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup, Tag
-from typing import TypedDict, Optional, Tuple, List
+from typing import Tuple
 from abc import ABC, abstractmethod
 
-
-class ExtractItems(ABC):
-    def __init__(self):
-        return
-    
-    @abstractmethod
-    def get_items(self) -> str:
-        raise NotImplementedError("Not implemented")
-    
-
-    @abstractmethod
-    def get_name(self) -> str:
-        raise NotImplementedError("Not implemented")
-    
-
-    @abstractmethod
-    def get_price_current(self) -> str:
-        raise NotImplementedError("Not implemented")
-
-
-    @abstractmethod
-    def get_items_link(self) -> str:
-        raise NotImplementedError("Not implemented")
-    
-
-    @abstractmethod
-    def get_extract_df(self) -> dict:
-        raise NotImplementedError("Not implemented")
 
 class Client:
     def __init__(self,) -> None:
@@ -49,20 +21,73 @@ class Client:
         return BeautifulSoup(txt, 'html.parser')
 
 
-class ExtractBootsMaleItems(ExtractItems):
-    def __init__(self, client: Client, host: str, path: str) -> None:
+class ExtractItems(ABC):
+    def __init__(self, client: Client, host: str, path: str):
         self.host = host
         self.path = path
         self.url = host + path
         self.client = client
         self.result = []
+    
+    @abstractmethod
+    def _get_pages_count(self) -> int:
+        raise NotImplementedError("Not implemented")
+    
+
+    @abstractmethod
+    def _get_items(self) -> str:
+        raise NotImplementedError("Not implemented")
+    
+
+    @abstractmethod
+    def _get_name(self) -> str:
+        raise NotImplementedError("Not implemented")
+    
+
+    @abstractmethod
+    def _get_price_current(self) -> str:
+        raise NotImplementedError("Not implemented")
 
 
-    def get_items(self, sp: BeautifulSoup) -> Tag:
+    @abstractmethod
+    def _get_items_link(self) -> str:
+        raise NotImplementedError("Not implemented")
+    
+
+    @abstractmethod
+    def get_extract_df(self) -> dict:
+        raise NotImplementedError("Not implemented")
+    
+
+    def get_extract_df(self):
+        pages = self._get_pages_count(self.url)
+        pages = 2
+
+        for i in range(1, pages):
+            url = self.url + f"page-{i}/"
+            sp = self.client.get_bs_by_url(url)
+            items = self._get_items(sp)
+            
+            for item in items:
+                name = self._get_name(item=item)
+                price, current = self._get_price_current(item=item)
+                link = self._get_items_link(item)
+                res = {
+                    "name": name,
+                    "price": price,
+                    "current": current,
+                    "link": self.host + str(link),
+                }
+                self.result.append(res)
+
+
+
+class ExtractBootsMaleItems(ExtractItems):
+    def _get_items(self, sp: BeautifulSoup) -> Tag:
         return sp.find('div', class_='Fkfp3V')
 
 
-    def get_name(self, item: Tag) -> str:
+    def _get_name(self, item: Tag) -> str:
         try:
             name = item.find('div', class_='ihuxuw').text
             name = name.replace("\u2009", " ").replace("\xa0", " ")
@@ -71,7 +96,7 @@ class ExtractBootsMaleItems(ExtractItems):
             return None
 
 
-    def get_price_current(self, item: Tag) -> Tuple[int, str]:
+    def _get_price_current(self, item: Tag) -> Tuple[int, str]:
         try:
             price = item.find('span', class_='MeSmTt').text
             price, current = price.split("\u2009")
@@ -82,7 +107,7 @@ class ExtractBootsMaleItems(ExtractItems):
             return None, None
 
 
-    def get_items_link(self, item: Tag) -> Tag:
+    def _get_items_link(self, item: Tag) -> Tag:
         try:
             tag = item.find('a', class_='it25hX')
             return tag.get('href')
@@ -90,34 +115,13 @@ class ExtractBootsMaleItems(ExtractItems):
             return None
 
 
-    def get_pages_count(self, url: str) -> int:
+    def _get_pages_count(self, url: str) -> int:
         try:
             sp = self.client.get_bs_by_url(url)
             return len(sp.find(id='select-page'))
         except Exception:
             return 1
 
-
-    def get_extract_df(self):
-        pages = self.get_pages_count(self.url)
-        pages = 2
-
-        for i in range(1, pages):
-            url = self.url + f"page-{i}/"
-            sp = self.client.get_bs_by_url(url)
-            items = self.get_items(sp)
-            
-            for item in items:
-                name = self.get_name(item=item)
-                price, current = self.get_price_current(item=item)
-                link = self.get_items_link(item)
-                res = {
-                    "name": name,
-                    "price": price,
-                    "current": current,
-                    "link": self.host + str(link),
-                }
-                self.result.append(res)
     
 
 client = Client()
